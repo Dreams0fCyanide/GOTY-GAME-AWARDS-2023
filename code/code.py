75f83be4614a3435216f986110d66e8960c85e49
Dreams0fCyanide <monstered99@gmail.com>
2022-09-10 23:09:18 +0200

    Eventy w menu

--- code/code.py
+++ code/code.py
@@ -1,5 +1,13 @@
 import pygame
-import pygame_menu
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
 from sys import exit
 import tkinter
 from tkinter import *
@@ -15,26 +23,43 @@ pygame.display.set_caption('Koteczki miłości')
 
 
 #Zmienne globalne
-dane = os.path.dirname(__file__)
+path = os.path.dirname(__file__)
+running = True
 
-ikona = pygame.image.load(os.path.join(dane, '../grafika/catpapiez.png'))
-pygame.display.set_icon(ikona)
+icon = pygame.image.load(os.path.join(path, '../grafika/catpapiez.png'))
+pygame.display.set_icon(icon)
 
 #menu
-imp = pygame.image.load(os.path.join(dane, '../grafika/koteczki.png'))
-scrn.blit(imp, (0, 0))
+bcgrnd = pygame.image.load(os.path.join(path, '../grafika/koteczki.png'))
+scrn.blit(bcgrnd, (0, 0))
 
+nowa_gra = pygame.Surface((410,79))  # szerokość, wysokość
+nowa_gra.set_alpha(0)                # przezroczysty = 0 wypełniony = 255
+scrn.blit(nowa_gra, (0,190))    # od lewej i od góry
+
+opcje = pygame.Surface((410,79))  # szerokość, wysokość
+opcje.set_alpha(0)                # przezroczysty = 0 wypełniony = 255
+scrn.blit(opcje, (0,320))    # od lewej i od góry
+
+wyjdz = pygame.Surface((410,79))  # szerokość, wysokość
+wyjdz.set_alpha(0)                # przezroczysty = 0 wypełniony = 255
+scrn.blit(wyjdz, (0,450))    # od lewej i od góry
 
 #Główna pętla
-while True:
+while running:
     for event in pygame.event.get():
+        pos = pygame.mouse.get_pos()
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            status = False  
+        elif event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            if wyjdz.collidepoint(pos):
+                pygame.quit()
+                exit()
 
         pygame.display.flip()
         clock.tick(60)
